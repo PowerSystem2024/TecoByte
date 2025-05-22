b320297b7179eb14b2c2f4c126ce5259027e3340
Luciana Fragueiro <100296143+lucianafragueiro@users.noreply.github.com>
2025-05-22 19:35:22 -0300

    Java Clase 7

---
+++ Java/Leccion6/Interfaces/src/accesodatos/IAccesoDatos.java
@@ -0,0 +1,10 @@
+package accesodatos;
+public interface IAccesoDatos {
+    int MAX_REGISTRO = 10;
+
+    //Metodo insertar es avstracto y sin cuerpo
+    void insertar();
+    void listar();
+    void actualizar();
+    void eliminar();
+}
