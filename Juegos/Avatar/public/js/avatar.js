806c0f8a3b51779ea26abe06ceb4c50fcd217063
Leandro Enriquez <enriquezleandro@gmail.com>
2025-06-08 15:49:32 -0300

    Avances en el juego de avatar
    Cambios en ataques

--- Juegos/Avatar/public/js/avatar.js
+++ Juegos/Avatar/public/js/avatar.js
@@ -1,8 +1,18 @@
+let ataqueJugador;
+let ataqueEnemigo;
+
 //Creamos una funcion que se ejecuta al cargar la pagina, en este caso lo que hace es agregar un evento al boton de seleccionar personaje
 function iniciarJuego() {
   //Aca creamos una variable para el boton y luego le agregamos un evento listener
   let botonPersonajeJugador = document.getElementById("boton-personaje");
   botonPersonajeJugador.addEventListener("click", seleccionarPersonajeJugador);
+
+  let botonTrompada = document.getElementById("boton-trompada");
+  botonTrompada.addEventListener("click", ataqueTrompada);
+  let botonPatada = document.getElementById("boton-patada");
+  botonPatada.addEventListener("click", ataquePatada);
+  let botonBarrida = document.getElementById("boton-barrida");
+  botonBarrida.addEventListener("click", ataqueBarrida);
 }
 
 // Con esta funcion lo que logramos es poder mostrar un alert con un mensaje predeterminado segun el label seleccionado
@@ -39,6 +49,30 @@ function seleccionarPersonajeEnemigo() {
   if (spanPersonajeEnemigo) {
     spanPersonajeEnemigo.innerHTML = enemigo.id;
   }
+}
+
+function ataqueTrompada() {
+  ataqueJugador = "Trompada";
+  ataqueAleatorioEnemigo();
+}
+
+function ataquePatada() {
+  ataqueJugador = "Patada";
+  ataqueAleatorioEnemigo();
+}
+
+function ataqueBarrida() {
+  ataqueJugador = "Barrida";
+  ataqueAleatorioEnemigo();
+}
+
+function ataqueAleatorioEnemigo() {
+  let ataques = ["Trompada", "Patada", "Barrida"];
+  ataqueEnemigo = ataques[aleatorio(1, ataques.length - 1)];
+}
+
+function aleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 window.addEventListener("load", iniciarJuego);
