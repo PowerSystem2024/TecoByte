2cda0d66d7bf77f7612dc54c1cf2c1627de5ca34
Maximiliano David Tomio <maxidpoggio@gmail.com>
2025-06-29 23:47:49 -0300

    Clase 11 Python
    Clase 11 Python

--- Python/Leccion07/capa_datos_persona/conexion.py
+++ Python/Leccion07/capa_datos_persona/conexion.py
@@ -52,8 +52,20 @@ def obtenerPool(cls):
         return cls._pool
 
 
+@classmethod
+def liberarConexion(cls, conexion):
+    cls.obtenerPool().putconn(conexion)
+    log.debug(f"Regresamos la conexión del pool: {conexion}")
+
+
+@classmethod
+def cerrarConexiones(cls):
+    cls.obtenerPool().closeall()
+
+
 if __name__ == "__main__":
     conexion1 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion1)
     conexion2 = Conexion.obtenerConexion()
     conexion3 = Conexion.obtenerConexion()
     conexion4 = Conexion.obtenerConexion()
